c96ec92a01e003e170bd4d3dba17d683d2348199
dlegland <david.legland@inrae.fr>
2021-11-29 19:14:12 +0100

    TopologicalTriMesh3D: update subdivide.m

--- meshes/@TopologicalTriMesh3D/subdivide.m
+++ meshes/@TopologicalTriMesh3D/subdivide.m
@@ -16,12 +16,15 @@ function res = subdivide(obj, n)
 % Created: 2021-10-27,    using Matlab 9.10.0.1684407 (R2021a) Update 3
 % Copyright 2021 INRAE.
 
-% compute the edge array
-ensureValidEdges(obj);
-ensureValidFaceEdges(obj);
+% % compute the edge array
+% ensureValidEdges(obj);
+% ensureValidFaceEdges(obj);
 
+% initialize result mesh
+res = TopologicalTriMesh3D();
+addVertex(res, obj.Vertices);
 % initialise the array of new vertices
-vertices2 = obj.Vertices;
+% vertices2 = obj.Vertices;
 
 % several interpolated positions
 t = linspace(0, 1, n + 1)';
@@ -29,7 +32,7 @@ coef2 = t(2:end-1);
 coef1 = 1 - t(2:end-1);
 
 % keep an array containing index of new vertices for each original edge
-nEdges = edgeNumber(obj);
+nEdges = edgeCount(obj);
 edgeNewVertexIndices = zeros(nEdges, n-1);
 
 % create new vertices on each edge
@@ -40,12 +43,13 @@ for iEdge = 1:nEdges
     % compute new points
     newPoints = coef1 * v1 + coef2 * v2;
     % add new vertices, and keep their indices
-    edgeNewVertexIndices(iEdge,:) = size(vertices2, 1) + (1:n-1);
-    vertices2 = [vertices2 ; newPoints]; %#ok<AGROW>
+%     edgeNewVertexIndices(iEdge,:) = size(vertices2, 1) + (1:n-1);
+%     vertices2 = [vertices2 ; newPoints]; %#ok<AGROW>
+    edgeNewVertexIndices(iEdge,:) = addVertex(res, newPoints);
 end
 
-faces2 = zeros(0, 3);
-nFaces = faceNumber(obj);
+% faces2 = zeros(0, 3);
+nFaces = faceCount(obj);
 for iFace = 1:nFaces
     % compute index of each corner vertex
     face = obj.Faces(iFace, :);
@@ -74,7 +78,8 @@ for iFace = 1:nFaces
     % create the first new face, on 'top' of the original face
     topVertexInds = [edge1NewVertexIndices(1) edge3NewVertexIndices(1)];
     newFace = [iv1 topVertexInds];
-    faces2 = [faces2; newFace]; %#ok<AGROW>
+%     faces2 = [faces2; newFace]; %#ok<AGROW>
+    addFace(res, newFace);
     
     % iterate over middle strips
     for iStrip = 2:n-1
@@ -83,8 +88,10 @@ for iFace = 1:nFaces
         ivr2 = edge3NewVertexIndices(iStrip);
         
         % extreme vertices as points
-        v1 = vertices2(ivr1, :);
-        v2 = vertices2(ivr2, :);
+        v1 = res.Vertices(ivr1,:);
+        v2 = res.Vertices(ivr2,:);
+%         v1 = vertices2(ivr1, :);
+%         v2 = vertices2(ivr2, :);
         
         % create additional vertices within the bottom row of the strip
         t = linspace(0, 1, iStrip+1)';
@@ -92,23 +99,27 @@ for iFace = 1:nFaces
         coef1 = 1 - t(2:end-1);
         newPoints = coef1 * v1 + coef2 * v2;
         
-        % compute indices of new vertices in result array
-        newInds = size(vertices2, 1) + (1:iStrip-1);
-        botVertexInds = [ivr1 newInds ivr2];
+%         % compute indices of new vertices in result array
+%         newInds = size(vertices2, 1) + (1:iStrip-1);
+%         botVertexInds = [ivr1 newInds ivr2];
         
         % add new vertices
-        vertices2 = [vertices2 ; newPoints]; %#ok<AGROW>
+%         vertices2 = [vertices2 ; newPoints]; %#ok<AGROW>
+        newInds = addVertex(res, newPoints);
+        botVertexInds = [ivr1 newInds ivr2];
         
         % create top faces of current strip
         for k = 1:iStrip-1
             newFace = [topVertexInds(k) botVertexInds(k+1) topVertexInds(k+1)];
-            faces2 = [faces2; newFace]; %#ok<AGROW>
+%             faces2 = [faces2; newFace]; %#ok<AGROW>
+            addFace(res, newFace);
         end
         
         % create bottom faces of current strip
         for k = 1:iStrip
             newFace = [topVertexInds(k) botVertexInds(k) botVertexInds(k+1)];
-            faces2 = [faces2; newFace]; %#ok<AGROW>
+%             faces2 = [faces2; newFace]; %#ok<AGROW>
+            addFace(res, newFace);
         end
         
         % bottom vertices of current strip are top vertices of next strip
@@ -126,14 +137,16 @@ for iFace = 1:nFaces
     % create top faces for last strip
     for k = 1:n-1
         newFace = [topVertexInds(k) botVertexInds(k+1) topVertexInds(k+1)];
-        faces2 = [faces2; newFace]; %#ok<AGROW>
+%         faces2 = [faces2; newFace]; %#ok<AGROW>
+        addFace(res, newFace);
     end
     
     % create bottom faces for last strip
     for k = 1:n
         newFace = [topVertexInds(k) botVertexInds(k) botVertexInds(k+1)];
-        faces2 = [faces2; newFace]; %#ok<AGROW>
+%         faces2 = [faces2; newFace]; %#ok<AGROW>
+        addFace(res, newFace);
     end
 end
 
-res = TopologicalTriMesh3D(vertices2, faces2);
+% res = TopologicalTriMesh3D(vertices2, faces2);
